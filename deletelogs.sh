7a5aced1dce41762bf8eadb1253271a1568c7c26
George AlMousa <61430834+georgealmousa@users.noreply.github.com>
2022-01-15 11:33:09 +0200

    Update deletelogs.sh

--- deletelogs.sh
+++ deletelogs.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-pathname1='/u01/app/oracle/diag/rdbms/*/'
-pathname2='/u01/app/oracle/admin/*/adump'
+pathname1='/u01/app/oracle/diag/rdbms/ecc/ecc/trace'
+pathname2='/u01/app/oracle/admin/ecc/adump'
 # Check the path is exists or not
 if [ -d "$pathname1" ]; then
-#   rm -rf "$pathname1"/*
    cd "$pathname1"
-   ls -lhA | awk -F " " '{print $9}' | sudo xargs -L 1 rm -rf
-   echo " Inside $pathname1 are removed"
+   ls -lhA | awk -F " " '{print $9}' | xargs -L 1 rm -rf
+   echo "Inside $pathname1 are removed"
+else
+   echo "$pathname1 path not found."
+fi
+if [ -d "$pathname2" ]; then
+    cd "$pathname2"
+    ls -lhA | awk -F " " '{print $9}' | xargs -L 1 rm -rf
+    echo "Inside $pathname2 are removed"
+else
+   echo "$pathname2 path not found."
 fi
